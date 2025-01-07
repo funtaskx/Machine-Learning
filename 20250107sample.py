3f7c9ebb144341de41d2bab76085e741cc0aa60a
Erenk <yche8605@uni.sydney.edu.au>
2025-01-07 19:06:12 +0800

    Initial commit

---
+++ 20250107sample.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+# read csv file
+df = pd.read_csv("biomedical_data.csv")
+
+# replace N/A
+df.fillna(df.median(), inplace=True)
+
+# standardize data
+scaler = StandardScaler()
+df_scaled = scaler.fit_transform(df.iloc[:, :-1])  # 假设最后一列是标签
+
+# classify training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(df_scaled, df['label'], test_size=0.2, random_state=42)
